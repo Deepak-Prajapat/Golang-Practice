2ca10570be69fcb1e0e11fcd2d79906908927545
Deepak-Prajapat <87295089+Deepak-Prajapat@users.noreply.github.com>
2021-12-02 12:34:53 +0530

    changed naming

--- Practice/HackerRank.go
+++ Practice/HackerRank.go
@@ -6,33 +6,33 @@ import (
 )
 
 func main() {
-
 	var time string = "11:59:00PM"
 	//var time string = "04:59:59AM"
 	var militaryTime string = timeConversion(time)
 	fmt.Println(militaryTime)
 }
 
-func timeConversion(s string) string {
 
-	amOrPm := s[len(s)-2 : len(s)]
-	hour := s[0:2]
-	var lengthWithoutAMPM = len(s) - 2
+/*
+ * It will convert 12Hour format time to 24 hour 
+ */
+func timeConversion(timeString string) string {
+	amOrPm := timeString[len(timeString)-2 : len(timeString)]
+	hour := timeString[0:2]
+	var lengthWithoutAMPM = len(timeString) - 2
 
 	var militaryTime string
 
 	if amOrPm == "PM" {
 		if hour == "12" {
-			militaryTime = s[0:lengthWithoutAMPM]
+			militaryTime = timeString[0:lengthWithoutAMPM]
 		} else if intHour, err := strconv.Atoi(hour); err == nil {
-			militaryTime = strconv.Itoa(intHour+12) + s[2:lengthWithoutAMPM]
+			militaryTime = strconv.Itoa(intHour+12) + timeString[2:lengthWithoutAMPM]
 		}
+	} else hour == "12" {
+		militaryTime = "00" + timeString[2:lengthWithoutAMPM]
 	} else {
-		if hour == "12" {
-			militaryTime = "00" + s[2:lengthWithoutAMPM]
-		} else {
-			militaryTime = s[0:lengthWithoutAMPM]
-		}
+		militaryTime = timeString[0:lengthWithoutAMPM]
 	}
 	return militaryTime
 }
